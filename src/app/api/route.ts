92d63e1598dccce64f92b2ff814631ae1e867147
Arno <surfacew@163.com>
2024-05-29 11:45:55 +0800

    :sparkles: (link-crawler) vercel supported link-crawler impl.

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,9 +1,14 @@
+import { load } from 'cheerio';
 import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
 const CHROMIUM_PATH =
   "https://vomrghiulbmrfvmhlflk.supabase.co/storage/v1/object/public/chromium-pack/chromium-v123.0.0-pack.tar";
+
+function removeAllEmptyLines(str: string) {
+  return str.replace(/^\s*\n/gm, '');
+}
 
 async function getBrowser() {
   if (process.env.VERCEL_ENV === "production") {
@@ -33,15 +38,60 @@ async function getBrowser() {
 }
 
 export async function GET(request: NextRequest) {
+
+  const searchParams = request.nextUrl.searchParams;
+  const url = searchParams.get("url") || '';
+
+  // WIP: auth for server invocation ak / sk for server invocations
+
+  console.info('🔗 user request content url:', url);
+
+  if (!url) {
+    return new Response("URL query parameter is required", { status: 400 });
+  }
+
   const browser = await getBrowser();
 
   const page = await browser.newPage();
-  await page.goto("https://example.com");
-  const pdf = await page.pdf();
-  await browser.close();
-  return new NextResponse(pdf, {
-    headers: {
-      "Content-Type": "application/pdf",
-    },
+  await page.goto(url, {
+    timeout: 30000, // 30s
+    waitUntil: 'load' // full page load
   });
+
+  // Query for an element handle.
+  let content = await page.content();
+
+  try {
+    const $ = await load(content);
+
+    $('script').remove();
+    $('link').remove();
+    $('style').remove();
+
+    let crawledText = 'empty';
+
+    const articleQuery = $('article');
+
+    if (articleQuery.length > 0) {
+      crawledText = articleQuery.text();
+    } else if ($('body main').length > 0) {
+      crawledText = $('body main')?.first().text();
+    } else {
+      crawledText = $('body').text();
+    }
+
+    crawledText = removeAllEmptyLines(crawledText);
+    console.info('🔗 crawled text:', crawledText);
+
+    // release the page from memory
+    await page.close();
+    await browser.close();
+
+    return NextResponse.json({
+      text: crawledText
+    });
+  } catch (e) {
+    console.error('🔗 error:', e);
+    return new Response("Error while crawling the content", { status: 500 });
+  }
 }
